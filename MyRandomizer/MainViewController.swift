7ad01db820887f8be67b4c31c1f58d0fc85e8e44
id2k1149 <id2k1149@gmail.com>
2022-10-29 20:07:46 -0400

    add logic to MainViewController

--- MyRandomizer/MainViewController.swift
+++ MyRandomizer/MainViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet var minValueLabel: UILabel!
+    @IBOutlet var maxValueLabel: UILabel!
+    @IBOutlet var randomNumberLabel: UILabel!
+    
+    @IBOutlet var getResultButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        getResultButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func getResultButtonTapped() {
+        let minimumNumber = Int(minValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(maxValueLabel.text ?? "") ?? 100
+        
+        randomNumberLabel.text = Int.random(in: minimumNumber...maximumNumber).formatted()
+    }
+    
 }
 
