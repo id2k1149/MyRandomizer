fb15ab3dffb2fee654e0cdc15cd73b08391e73e9
id2k1149 <id2k1149@gmail.com>
2022-11-06 11:16:05 -0500

    add struct RandomNumber

--- MyRandomizer/Models/RandomNumber.swift
+++ MyRandomizer/Models/RandomNumber.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct RandomNumber {
+    var minimumValue: Int
+    var maximumValue: Int
+    
+    var getRandom: Int {
+        Int.random(in: minimumValue...maximumValue)
+    }
+}
